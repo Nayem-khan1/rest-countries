bd2305e4a8b4a969ea8bc2f975c4ef0358b7deb6
Nayem-khan1 <nayemkhan4496@gmail.com>
2023-01-11 00:44:28 +0600

    fix bug

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -9,6 +9,10 @@ export const router = createBrowserRouter([
         path:'/',
         element: <Main></Main>, children:[
             {
+                path:'/',
+                element: <Home></Home>
+            },
+            {
                 path:'home',
                 element: <Home></Home>
             },
